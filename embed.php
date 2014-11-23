6914a39f7d19c7e5d5f89dc233569cae9821eed2
Richard <richard.be.jamin@gmail.com>
2014-11-22 19:44:58 -0600

    fixed emebed

--- embed.php
+++ embed.php
@@ -17,21 +17,41 @@
   getenv('HTTP_FORWARDED_FOR')?:
   getenv('HTTP_FORWARDED')?:
   getenv('REMOTE_ADDR');
-  error_log( ip2long($ip) );
   $user = array(
     'ip'      => $ip,
     'id'      => $_COOKIE['PHPSESSID'],
     'agent'   => $HTTP_USER_AGENT,
     'time'    => $REQUEST_TIME_FLOAT,
     'scheme'  => $REQUEST_SCHEME,
-    'host'    => $HTTP_HOST,
-    'url'     => $HTTP_REFERER,
+    'host'    => '',
+    'url'     => '',
     'city'    => '',
     'state'   => '',
     'lat'     => '',
-    'lng'     => ''
+    'lng'     => '',
+    'screen_dimensions' => '',
+    'browser_dimensions' => ''
   );
   $user = json_encode($user);
 ?>
 <script src="<?php echo base_url; ?>/js/build/client.min.js"></script>
-<script>var socket = io('<?php echo io_url; ?>');socket.on('connect', function () {socket.emit('client-info', <?php echo $user; ?>);});</script>
+<script>
+  var user = <?php echo $user; ?>;
+
+  var url = document.referrer;
+  var tmp = document.createElement('a');
+  tmp.href = url;
+  var host = tmp.hostname;
+
+  user.host = host;
+  user.url = url;
+  user.screen_dimensions = screen.width + ' x ' + screen.height;
+  user.browser_dimensions = window.outerWidth + ' x ' + window.outerHeight;
+
+
+  // console.log( user );
+  var socket = io('<?php echo io_url; ?>');
+  socket.on('connect', function () {
+    socket.emit('client-info', user);
+  });
+  </script>
